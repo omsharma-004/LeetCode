98460fd56a98772f95503375476774b6c57e2d6a
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-09-11 11:13:56 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (87.45%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -1,27 +1,47 @@
+// class Solution {
+// public:
+//     int levels(TreeNode* root){
+//         if(root==NULL) return 0;
+//         return 1+max(levels(root->left),levels(root->right));
+//     }
+//     void nlevel(TreeNode* root, int curr, int level, vector<int>& ans){
+//         if(root==NULL) return;
+//         if(curr==level){
+//             ans[curr]=root->val;
+//             return;
+//         }
+//         nlevel(root->left, curr+1, level, ans);
+//         nlevel(root->right, curr+1, level, ans);
+//     }
+//     void norder(TreeNode* root, vector<int>& ans){
+//         int n=ans.size();
+//         for(int i=0;i<n;i++){
+//             nlevel(root, 0, i, ans);
+//         }
+//     }
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector<int> ans(levels(root),0);
+//         norder(root, ans);
+//         return ans;
+//     }
+// };
+
+//Using preorder technique
 class Solution {
 public:
     int levels(TreeNode* root){
         if(root==NULL) return 0;
         return 1+max(levels(root->left),levels(root->right));
     }
-    void nlevel(TreeNode* root, int curr, int level, vector<int>& ans){
+    void pre(TreeNode* root, vector<int>& ans, int lvl){
         if(root==NULL) return;
-        if(curr==level){
-            ans[curr]=root->val;
-            return;
-        }
-        nlevel(root->left, curr+1, level, ans);
-        nlevel(root->right, curr+1, level, ans);
-    }
-    void norder(TreeNode* root, vector<int>& ans){
-        int n=ans.size();
-        for(int i=0;i<n;i++){
-            nlevel(root, 0, i, ans);
-        }
+        ans[lvl]=root->val;
+        pre(root->left, ans, lvl+1);
+        pre(root->right, ans, lvl+1);
     }
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans(levels(root),0);
-        norder(root, ans);
+        pre(root, ans,0);
         return ans;
     }
 };
