900a38882086dd3250900552bf0765bf1d520684
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-09-13 20:55:44 +0530

    Time: 14 ms (12.62%) | Memory: 20.5 MB (54.30%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,17 +1,40 @@
+// class Solution {
+// public:
+//     long long minbst(TreeNode* root){
+//         if(root==NULL) return LLONG_MAX;
+//         else return min((long long)(root->val),min(minbst(root->left),minbst(root->right)));
+//     }
+//     long long maxbst(TreeNode* root){
+//         if(root==NULL) return LLONG_MIN;
+//         else return max((long long)(root->val),max(maxbst(root->left),maxbst(root->right)));
+//     }
+//     bool isValidBST(TreeNode* root) {
+//         if(root==NULL) return true;
+//         else if((long long)(root->val) >= minbst(root->right)) return false;
+//         else if((long long)(root->val) <= maxbst(root->left)) return false;
+//         return isValidBST(root->left) && isValidBST(root->right);
+//     }
+// };
+
+//second solution less complexity
 class Solution {
 public:
-    long long minbst(TreeNode* root){
-        if(root==NULL) return LLONG_MAX;
-        else return min((long long)(root->val),min(minbst(root->left),minbst(root->right)));
-    }
-    long long maxbst(TreeNode* root){
-        if(root==NULL) return LLONG_MIN;
-        else return max((long long)(root->val),max(maxbst(root->left),maxbst(root->right)));
+    void check(TreeNode* root, TreeNode* &prev, bool &flag){
+        if(root==NULL) return;
+        check(root->left, prev, flag);
+        if(prev!=NULL){
+            if(root->val<=prev->val){
+                flag=false;
+                return;
+            }
+        }
+        prev=root;
+        check(root->right, prev, flag);
     }
     bool isValidBST(TreeNode* root) {
-        if(root==NULL) return true;
-        else if((long long)(root->val) >= minbst(root->right)) return false;
-        else if((long long)(root->val) <= maxbst(root->left)) return false;
-        return isValidBST(root->left) && isValidBST(root->right);
+        TreeNode* prev=NULL;
+        bool flag=true;
+        check(root, prev, flag);
+        return flag;
     }
 };
