3a44e8e01ad90c82786660e44fa4ccdd6351d059
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-09-14 23:46:11 +0530

    Time: 8 ms (18.81%) | Memory: 15.9 MB (98.77%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,23 +1,51 @@
+// class Solution {
+// public:
+//     TreeNode* build(vector<int>& pre, int prelo, int prehi, vector<int>& in, int inlo, int inhi){
+//         if(prelo>prehi) return NULL;
+//         TreeNode* root= new TreeNode(pre[prelo]);
+//         if(prelo==prehi) return root;
+//         int i=inlo;
+//         while(i<inhi){
+//             if(in[i]==pre[prelo]) break;
+//             i++;
+//         }
+//         int leftcount=i-inlo;
+//         root->left=build(pre, prelo+1, prelo+leftcount, in, inlo, i-1);
+//         root->right=build(pre, prelo+leftcount+1, prehi, in, i+1, inhi);
+//         return root;
+//     }
+//     TreeNode* bstFromPreorder(vector<int>& preorder) {
+//         vector<int> help=preorder;
+//         sort(help.begin(), help.end());
+//         int n=preorder.size();
+//         return build(preorder, 0, n-1, help, 0, n-1);
+//     }
+// };
+
+//using insertion for loop
 class Solution {
 public:
-    TreeNode* build(vector<int>& pre, int prelo, int prehi, vector<int>& in, int inlo, int inhi){
-        if(prelo>prehi) return NULL;
-        TreeNode* root= new TreeNode(pre[prelo]);
-        if(prelo==prehi) return root;
-        int i=inlo;
-        while(i<inhi){
-            if(in[i]==pre[prelo]) break;
-            i++;
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL) return new TreeNode(val);
+        else if(root->val>val){
+            if(root->left==NULL){
+                root->left=new TreeNode(val);
+            }
+            else insertIntoBST(root->left, val);
         }
-        int leftcount=i-inlo;
-        root->left=build(pre, prelo+1, prelo+leftcount, in, inlo, i-1);
-        root->right=build(pre, prelo+leftcount+1, prehi, in, i+1, inhi);
+        else{
+            if(root->right==NULL){
+                root->right=new TreeNode(val);
+            }
+            else insertIntoBST(root->right, val);
+        }  
         return root;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        vector<int> help=preorder;
-        sort(help.begin(), help.end());
-        int n=preorder.size();
-        return build(preorder, 0, n-1, help, 0, n-1);
+        TreeNode* root=new TreeNode(preorder[0]);
+        for(int i=1;i<preorder.size();i++){
+            insertIntoBST(root,preorder[i]);
+        }
+        return root;
     }
 };
