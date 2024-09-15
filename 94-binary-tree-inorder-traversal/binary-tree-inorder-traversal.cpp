f7367790c77de8716976611b22b5dd159ac11933
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-09-15 14:03:27 +0530

    Time: 5 ms (7.03%) | Memory: 9.9 MB (87.86%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -14,22 +14,53 @@
 // };
 
 // Iterative solution
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         stack<TreeNode*> st;
+//         TreeNode* node=root;
+//         while(st.size()>0 || node!=NULL){
+//             if(node!=NULL){
+//                 st.push(node);
+//                 node=node->left;
+//             }
+//             else{
+//                 TreeNode* temp=st.top();
+//                 st.pop();
+//                 ans.push_back(temp->val);
+//                 node=temp->right;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+//MORRIS TRAVERSAL
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        stack<TreeNode*> st;
-        TreeNode* node=root;
-        while(st.size()>0 || node!=NULL){
-            if(node!=NULL){
-                st.push(node);
-                node=node->left;
+        TreeNode* curr= root;
+        while(curr!=NULL){
+            if(curr->left!=NULL){
+                TreeNode* pre=curr->left;
+                while(pre->right!=NULL && pre->right!=curr){
+                    pre=pre->right;
+                }
+                if(pre->right==NULL){
+                    pre->right=curr;
+                    curr= curr->left;
+                }
+                else{
+                    pre->right=NULL;
+                    ans.push_back(curr->val);
+                    curr=curr->right;
+                }
             }
             else{
-                TreeNode* temp=st.top();
-                st.pop();
-                ans.push_back(temp->val);
-                node=temp->right;
+                ans.push_back(curr->val);
+                curr=curr->right;
             }
         }
         return ans;
