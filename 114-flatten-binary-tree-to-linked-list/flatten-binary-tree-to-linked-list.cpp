aebca7f83a291b590b7959e0abf9e896ca82eb0f
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-09-16 23:30:58 +0530

    Time: 3 ms (78.63%) | Memory: 16.9 MB (46.68%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -17,22 +17,50 @@
 //     }
 // };
 
-//Morris usage
+//O(N) space
+// class Solution {
+// public:
+//     void flatten(TreeNode* root) {
+//         if(root==NULL) return;
+//         TreeNode* l=root->left;
+//         TreeNode* r=root->right;
+//         root->left=NULL;
+//         root->right=NULL;
+//         flatten(l);
+//         flatten(r);
+//         TreeNode* temp=root;
+//         root->right=l;
+//         while(temp->right!=NULL){
+//             temp=temp->right;
+//         }
+//         temp->right=r;
+//     }
+// };
+
+//Moriis 
+
 class Solution {
 public:
     void flatten(TreeNode* root) {
         if(root==NULL) return;
-        TreeNode* l=root->left;
-        TreeNode* r=root->right;
-        root->left=NULL;
-        root->right=NULL;
-        flatten(l);
-        flatten(r);
+        TreeNode* curr=root;
+        while(curr!=NULL){
+            if(curr->left!=NULL){
+                TreeNode* r=curr->right;
+                curr->right=curr->left;
+                TreeNode* pred=curr->left;
+                while(pred->right!=NULL) pred=pred->right;
+                pred->right=r;
+                curr=curr->left;
+            }
+            else{
+                curr=curr->right;
+            }
+        }
         TreeNode* temp=root;
-        root->right=l;
         while(temp->right!=NULL){
+            temp->left=NULL;
             temp=temp->right;
         }
-        temp->right=r;
     }
 };
