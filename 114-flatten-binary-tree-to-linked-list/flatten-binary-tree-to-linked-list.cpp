a39df7edfcc1dc75b13e68f3ad07309553906f1c
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-09-16 22:30:54 +0530

    Time: 5 ms (51.94%) | Memory: 16.9 MB (46.68%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -1,18 +1,38 @@
+// class Solution {
+// public:
+//     void helper(TreeNode* root, vector<TreeNode*>& ans){
+//         if(root==NULL) return;
+//         ans.push_back(root);
+//         helper(root->left, ans);
+//         helper(root->right, ans);
+//     }
+//     void flatten(TreeNode* root) {
+//         vector<TreeNode*> ans;
+//         helper(root,ans);
+//         int n=ans.size();
+//         for(int i=0;i<n-1;i++){
+//             ans[i]->right=ans[i+1];
+//             ans[i]->left=NULL;
+//         }
+//     }
+// };
+
+//Morris usage
 class Solution {
 public:
-    void helper(TreeNode* root, vector<TreeNode*>& ans){
-        if(root==NULL) return;
-        ans.push_back(root);
-        helper(root->left, ans);
-        helper(root->right, ans);
-    }
     void flatten(TreeNode* root) {
-        vector<TreeNode*> ans;
-        helper(root,ans);
-        int n=ans.size();
-        for(int i=0;i<n-1;i++){
-            ans[i]->right=ans[i+1];
-            ans[i]->left=NULL;
+        if(root==NULL) return;
+        TreeNode* l=root->left;
+        TreeNode* r=root->right;
+        root->left=NULL;
+        root->right=NULL;
+        flatten(l);
+        flatten(r);
+        TreeNode* temp=root;
+        root->right=l;
+        while(temp->right!=NULL){
+            temp=temp->right;
         }
+        temp->right=r;
     }
 };
