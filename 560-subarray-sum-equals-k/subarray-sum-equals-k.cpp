0050570c640a5b85f7b9b2820fadea418cfae0f1
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-09-22 22:21:12 +0530

    Time: 57 ms (89.36%) | Memory: 45.2 MB (69.68%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,16 +1,39 @@
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int count = 0;
+//         for (int i = 0; i < n; i++) {
+//             int currentSum = 0;
+//             for (int j = i; j < n; j++) {
+//                 currentSum += nums[j];
+//                 if (currentSum == k) {
+//                     ++count;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+//Using unordered map
+
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            int currentSum = 0;
-            for (int j = i; j < n; j++) {
-                currentSum += nums[j];
-                if (currentSum == k) {
-                    ++count;
-                }
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            nums[i]+=nums[i-1];
+        }
+        int count=0;
+        unordered_map<int ,int> mp;
+        for(int i=0;i<n;i++){
+            if(nums[i]==k){
+                count++;
             }
+            int rem=nums[i]-k;
+            if(mp.find(rem)!=mp.end()) count+=mp[rem];
+            mp[nums[i]]++;
         }
         return count;
     }
