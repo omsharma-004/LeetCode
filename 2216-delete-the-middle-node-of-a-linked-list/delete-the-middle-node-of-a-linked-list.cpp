3639894538dd52b02e70318b7053155a248c8737
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-10-10 01:26:12 +0530

    Time: 542 ms (99.11%) | Memory: 299.9 MB (6.57%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         if(head==NULL || head->next==NULL) return NULL;
         ListNode* slow=head;
         int i=1;
