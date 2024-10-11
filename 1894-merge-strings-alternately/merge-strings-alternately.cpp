75c1f6d50688d4c384a60dfefc27c894cf3f1ab7
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-10-12 00:11:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (77.32%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     string mergeAlternately(string words1, string words2) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         string a="";
         int i=0;
         int j=0;
