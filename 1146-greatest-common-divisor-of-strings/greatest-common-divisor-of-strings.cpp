1995b26c3e28b2857bdda3b50e980ffa3bc63c49
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-10-12 00:26:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (30.32%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         if(str1 + str2 != str2 + str1) return "";
         return str1.substr(0, gcd(str1.size(), str2.size()));
     }
