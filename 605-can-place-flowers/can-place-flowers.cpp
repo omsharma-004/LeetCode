799ba971175b18a186ea45129beed8db488fe857
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-10-11 15:03:17 +0530

    Time: 8 ms (82.77%) | Memory: 23.1 MB (7.06%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& arr, int n) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         for(int i=0;i<arr.size();i++){
             if (arr[i] == 0 && (i == 0 || arr[i - 1] == 0) && (i == arr.size() - 1 || arr[i + 1] == 0)){
                 arr[i]=1;
