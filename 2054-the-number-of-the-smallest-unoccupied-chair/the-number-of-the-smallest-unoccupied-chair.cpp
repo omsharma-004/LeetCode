60ae5c4bd7371cf75e8c31ff2a66c1555d1d97bd
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-10-11 22:12:51 +0530

    Time: 120 ms (97.59%) | Memory: 61.5 MB (70.45%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,6 +1,9 @@
 class Solution {
  public:
   int smallestChair(vector<vector<int>>& times, int targetFriend) {
+    ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     int nextUnsatChair = 0;
     priority_queue<int, vector<int>, greater<>> emptyChairs;
     using P = pair<int, int>;  // (leaving, chair)
