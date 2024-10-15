3c28440e8f2a403567e998fc3d98e445b28f98ad
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-10-16 01:27:57 +0530

    Time: 23 ms (94.60%) | Memory: 15.8 MB (10.58%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -1,15 +1,14 @@
 class Solution {
  public:
   long long minimumSteps(string s) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     long fast = 0;
     int slow = 0;
-
     for (auto c : s)
-      if (c == '1')
-        slow++;
-      else
-        fast+=slow;
-
+      if (c == '1') slow++;
+      else fast+=slow;
     return fast;
   }
 };
