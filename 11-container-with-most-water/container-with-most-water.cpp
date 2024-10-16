99b8d7474a3d48e43ba32205cf6aec533d1da9fc
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-10-17 01:14:32 +0530

    Time: 43 ms (97.67%) | Memory: 62.1 MB (27.54%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -4,14 +4,14 @@ public:
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
-        int ans = 0;
+        int ans = INT_MIN;
         int l = 0;
         int r = height.size()-1;
         while(l<r){
             int minh = min(height[l],height[r]);
             ans = max(ans, minh*(r-l));
-            if(height[l]<height[r]) {++l;}
-            else{--r;}
+            if(height[l]<height[r]) l++;
+            else --r;
         }
         return ans;
     }
