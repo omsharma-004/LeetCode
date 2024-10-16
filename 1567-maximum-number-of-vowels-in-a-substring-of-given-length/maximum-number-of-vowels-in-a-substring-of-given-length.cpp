a1342b7519d9886f221a58f903b31513a1af9287
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-10-17 01:41:07 +0530

    Time: 25 ms (32.71%) | Memory: 11.8 MB (12.57%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int ans = 0;
         int mx = 0;
         unordered_set<char> s1={'a', 'e', 'i', 'o', 'u'};
