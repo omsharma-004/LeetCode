4c106494564eebb27ad4b86c13252234846e2b53
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-10-17 20:36:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (12.29%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int maximumSwap(int num) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         string str = to_string(num);
         vector<int> ans(10, -1);
         for (int i=0;i<str.size();i++) {
