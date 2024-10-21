e96125730d9c205a1405bddf53993286edbb2e79
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-10-21 10:57:08 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (24.54%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -27,6 +27,9 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         if (nums.size() <= 2) return nums.size();
         int index = 2;
         for (int i = 2; i < nums.size(); i++) {
