3e8fc32fced2f4a85e32b6ed6a8e5300d47a0206
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-10-21 22:01:23 +0530

    Time: 217 ms (66.37%) | Memory: 47.9 MB (66.67%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int maxUniqueSplit(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         unordered_set<string> seen;
         return backtrack(0, s, seen);
     }
