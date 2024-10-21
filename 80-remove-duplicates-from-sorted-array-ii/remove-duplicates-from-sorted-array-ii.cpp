9bbf27e32c7f3857d97e38f097cf474980603b18
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-10-21 10:56:45 +0530

    Time: 6 ms (26.95%) | Memory: 13.6 MB (70.26%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,25 +1,40 @@
+// class Solution {
+// public:
+//     int removeDuplicates(std::vector<int>& nums) {
+//         ios_base::sync_with_stdio(0);
+//         cin.tie(0);
+//         cout.tie(0);
+//         unordered_map<int, int> mp;
+//         for (auto i : nums) {
+//             mp[i]++;
+//         }
+//         vector<int> uniqueElements;
+//         for (const auto& entry : mp) {
+//             uniqueElements.push_back(entry.first);
+//         }
+//         int c=0;
+//         sort(uniqueElements.begin(), uniqueElements.end());
+//         for (const auto& num : uniqueElements) {
+//             int occurrences = std::min(mp[num], 2);
+//             for (int j = 0; j < occurrences; j++) {
+//                 nums[c++] = num;
+//             }
+//         }
+//         return c;
+//     }
+// };
+
 class Solution {
 public:
-    int removeDuplicates(std::vector<int>& nums) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        unordered_map<int, int> mp;
-        for (auto i : nums) {
-            mp[i]++;
-        }
-        vector<int> uniqueElements;
-        for (const auto& entry : mp) {
-            uniqueElements.push_back(entry.first);
-        }
-        int c=0;
-        sort(uniqueElements.begin(), uniqueElements.end());
-        for (const auto& num : uniqueElements) {
-            int occurrences = std::min(mp[num], 2);
-            for (int j = 0; j < occurrences; j++) {
-                nums[c++] = num;
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() <= 2) return nums.size();
+        int index = 2;
+        for (int i = 2; i < nums.size(); i++) {
+            if (nums[i] != nums[index - 2]) {
+                nums[index] = nums[i];
+                index++;
             }
         }
-        return c;
+        return index;
     }
 };
