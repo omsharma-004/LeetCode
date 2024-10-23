e713ee79bd52a3d936b104e5e1fe9f0bcc4ea928
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-10-23 11:32:41 +0530

    Time: 1 ms (81.59%) | Memory: 10.3 MB (44.68%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         stringstream ss(s);
         vector<string> words;
         string word;
