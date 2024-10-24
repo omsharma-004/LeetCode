deea954da24a3ecce2f7a3a3e95997efaa7070e7
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2024-10-24 14:40:32 +0530

    Time: 3 ms (75.00%) | Memory: 14.2 MB (11.14%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int compress(std::vector<char>& s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         vector<char> ans;
         int n = s.size();
         int i = 0;
