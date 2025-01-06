ad0470242faf58a69d03423b9d21a2d0e42bedfc
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-06 14:08:43 +0530

    Time: 151 ms (5.05%) | Memory: 12.1 MB (50.97%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-    vector<int> minOperations(string boxes) {
-        int n = boxes.size();
-        vector<int> leftCost(n), leftCount(n);
-        vector<int> rightCost(n), rightCount(n);
-        vector<int> res;
-        
-        for(int i=1; i<n; i++){
-            if(boxes[i-1] == '1')
-                leftCount[i] = leftCount[i-1]+1;
-            else 
-                leftCount[i] = leftCount[i-1];
-            leftCost[i] = leftCost[i-1]+leftCount[i];
+    vector<int> minOperations(string b) {
+        vector<int> ans(b.length(), 0);
+        for(int i = 0; i<b.length(); i++)
+        {
+            for(int j = 0; j<b.length(); j++)
+            {
+                if(i!=j && b[j]!='0')
+                {
+                    ans[i]+=abs(j-i);
+                }
+            }
         }
-        
-        for(int i=n-2; i>=0; i--){
-            if(boxes[i+1] == '1')
-                rightCount[i] = rightCount[i+1]+1;
-            else 
-                rightCount[i] = rightCount[i+1];
-            rightCost[i] = rightCost[i+1]+rightCount[i];
-        }
-        
-        for(int i=0; i<n; i++){
-            res.push_back(leftCost[i]+rightCost[i]);
-        }
-        
-        return res;
+
+        return ans;
+
     }
 };
