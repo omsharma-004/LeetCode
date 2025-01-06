2912e68bf9c4c4e4dd938a71b42a98c2b1ea5701
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-06 14:10:33 +0530

    Time: 155 ms (5.05%) | Memory: 12.2 MB (50.97%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> minOperations(string b) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         vector<int> ans(b.length(), 0);
         for(int i = 0; i<b.length(); i++)
         {
