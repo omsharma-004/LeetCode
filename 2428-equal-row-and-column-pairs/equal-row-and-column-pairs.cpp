f5f2a44b08b0ace17f064eba09c44f2b1d0cbaa2
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-10 15:14:07 +0530

    Time: 19 ms (62.71%) | Memory: 35.7 MB (64.67%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& arr) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int n=arr.size();
         int count=0;
         map<vector<int>, int> mp;
