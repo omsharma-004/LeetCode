0e77ca6d5f2648d14d65fc11f2596edf708db261
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-10 22:09:06 +0530

    Time: 23 ms (22.07%) | Memory: 14.5 MB (20.63%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,43 +1,20 @@
-// class Solution {
-// public:
-//     int lengthOfLongestSubstring(string s) {
-//         int st=0;
-//         int count=0;
-//         int fast=0;
-//         for(int i=1;i<s.length();i++){
-//             if(s[st]!=s[i] && s[fast]!=s[i]){
-//                 count++;
-//                 i++;
-//                 st++;
-//             }
-//             else{
-//                 st++;
-//                 fast++;
-//             }
-//         }
-//         return count+1;
-//     }
-// };
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<char> seen; // To store characters in the current window
-        int start = 0; // Start of the window
-        int maxLength = 0; // To store the maximum length
-        
-        for (int end = 0; end < s.length(); end++) {
-            // If the character at 'end' is in the window, move 'start' forward
-            while (seen.find(s[end]) != seen.end()) {
-                seen.erase(s[start]);
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        unordered_set<char> set;
+        int start = 0;
+        int maxLength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            while (set.find(s[i]) != set.end()) {
+                set.erase(s[start]);
                 start++;
             }
-            // Add the current character to the window
-            seen.insert(s[end]);
-            
-            // Update the maximum length
-            maxLength = max(maxLength, end - start + 1);
+            set.insert(s[i]);
+            maxLength = max(maxLength, i - start + 1);
         }
-        
         return maxLength;
     }
 };
