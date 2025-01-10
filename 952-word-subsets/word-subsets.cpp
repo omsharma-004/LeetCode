dc425ebd57a9fab0bf249f0f0b5852f6cf3055c9
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-10 14:06:38 +0530

    Time: 335 ms (8.68%) | Memory: 166.3 MB (10.50%) - LeetSync

---
+++ 952-word-subsets/word-subsets.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        unordered_map<char, int> mp1;
+        for (const string& word : words2) {
+            unordered_map<char, int> mp2;
+            for (char c : word) {
+                mp2[c]++;
+            }
+            for (const auto& entry : mp2) {
+                mp1[entry.first] =
+                    max(mp1[entry.first], entry.second);
+            }
+        }
+        vector<string> result;
+        for (const string& word : words1) {
+            unordered_map<char, int> mp3;
+            for (char c : word) {
+                mp3[c]++;
+            }
+            bool isSubset = true;
+            for (const auto& entry : mp1) {
+                if (mp3[entry.first] < entry.second) {
+                    isSubset = false;
+                    break;
+                }
+            }
+            if (isSubset) {
+                result.push_back(word);
+            }
+        }
+        return result;
+    }
+};
