d172ab7150d00e4e0b3aa526e0313e6208e4bddd
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-10 17:38:43 +0530

    Time: 73 ms (98.29%) | Memory: 88.5 MB (21.84%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
@@ -12,6 +12,9 @@ public:
         return left+right+r;
     }
     int goodNodes(TreeNode* root) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         return helper(root, 0, INT_MIN);
     }
 };
