c028b9c11df274af21cc76424692a84f7f73c28b
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-12 22:18:28 +0530

    Time: 12 ms (68.27%) | Memory: 30.4 MB (31.98%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int n = s.length();
         if (n % 2 != 0) {
             return false;
