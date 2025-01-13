e1f880cfad9b8ee28a14debe1b4b91d2d1b70672
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-13 23:10:29 +0530

    Time: 38 ms (55.24%) | Memory: 32.7 MB (21.37%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int minimumLength(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int n = s.length(), del = 0;
         unordered_map<char, int> mp;
         for(int i = 0; i < n; i++){
