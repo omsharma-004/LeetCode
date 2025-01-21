977d1d4196f75c6cfe45b8920eb949b8d19ecba8
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-21 19:18:30 +0530

    Time: 28 ms (5.11%) | Memory: 14.2 MB (64.65%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         long long sum=0;
         for(int i=0;i<nums.size();i++)
         {
