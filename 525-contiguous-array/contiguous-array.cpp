9a4ef31b4da61a38fa82a6c36d42fa0fa32ae4ca
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-23 10:52:47 +0530

    Time: 19 ms (84.51%) | Memory: 88.1 MB (90.43%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -4,22 +4,25 @@ public:
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
-       int cnt = 0;
-       int ans = 0;
-       int n = nums.size();
-       unordered_map<int,int>mp;
-       for(int i=0;i<n;i++){
-           int val = 0;
-           if(nums[i] == 0)cnt--;
-           if(nums[i] == 1)cnt++;
-           if(cnt == 0)val = i+1;
-           else if(mp.find(cnt) == mp.end()){
+        int cnt = 0;
+        int ans = 0;
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            int val = 0;
+            if (nums[i] == 0)
+                cnt--;
+            if (nums[i] == 1)
+                cnt++;
+            if (cnt == 0)
+                val = i + 1;
+            else if (mp.find(cnt)!=mp.end()) {
+                val = i - mp[cnt];
+            } else {
                 mp[cnt] = i;
-           }else{
-               val = i - mp[cnt];
-           }
-           ans = max(ans,val);
-       }
-       return ans;
+            }
+            ans = max(ans, val);
+        }
+        return ans;
     }
 };
