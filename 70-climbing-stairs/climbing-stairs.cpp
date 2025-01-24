1eca55badb51baf61e231a51b10c298902a7372e
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-24 10:28:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.16%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int climbStairs(int n) {
         vector<int> memo(n + 1, -1);
-        return climbStairsRecursive(n, memo);
+        return helper(n, memo);
     }
-    int climbStairsRecursive(int n, vector<int>& memo) {
+    int helper(int n, vector<int>& memo) {
         if (n <= 1) return 1;    
         if (memo[n] != -1) {
             return memo[n];
         }
-        memo[n] = climbStairsRecursive(n - 1, memo) + climbStairsRecursive(n - 2, memo);
+        memo[n] = helper(n - 1, memo) + helper(n - 2, memo);
         return memo[n];
     }
 };
