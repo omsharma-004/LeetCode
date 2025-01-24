9525865f88c9da0e78088f041c9c94f98ea86929
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-24 11:37:40 +0530

    Time: 35 ms (50.05%) | Memory: 18.9 MB (56.33%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int helper(int n, vector<int>& dp, vector<int>& coins){
+        if(n==0) return 0;
+        if(n<0) return INT_MAX;
+        if(dp[n]!=-1) return dp[n];
+        int minc=INT_MAX;
+        for(auto i:coins){
+            if(n-i>=0){
+                int temp=helper(n-i,dp,coins);
+                if(temp!=INT_MAX) minc=min(minc,temp);
+            }
+        }
+        if (minc==INT_MAX) dp[n]=INT_MAX;
+        else dp[n]=minc+1;
+        return dp[n];
+    }
+    int coinChange(vector<int>& coins, int n) {
+        vector<int> dp(n+1,-1);
+        int a= helper(n, dp, coins);
+        if(a==INT_MAX) return -1;
+        return a;
+    }
+};
