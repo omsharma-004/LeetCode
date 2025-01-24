79f3c833a92cfe5ede958bc92098e33a67e8db77
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-24 11:46:00 +0530

    Time: 13 ms (95.21%) | Memory: 18 MB (67.65%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
-    int helper(int n, vector<int>& dp, vector<int>& coins) {
-        if (n == 0)
-            return 0;
-        if (n < 0)
-            return INT_MAX;
-        if (dp[n] != -1)
-            return dp[n];
-        int minc = INT_MAX;
-        for (auto i : coins) {
-            if (n - i >= 0) {
-                int temp = helper(n - i, dp, coins);
-                if (temp != INT_MAX)
-                    minc = min(minc, temp);
+    int coinChange(vector<int>& coins, int n) {
+        vector<int> dp(n + 1, n + 1);
+        dp[0] = 0;
+        for (int i : coins) {
+            for (int j = i; j <= n; j++) {
+                dp[j] = min(dp[j], dp[j - i] + 1);
             }
         }
-        if (minc == INT_MAX)
-            dp[n] = INT_MAX;
-        else
-            dp[n] = minc + 1;
-        return dp[n];
-    }
-    int coinChange(vector<int>& coins, int n) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        vector<int> dp(n + 1, -1);
-        int a = helper(n, dp, coins);
-        if (a == INT_MAX)
+        if (dp[n] == n + 1)
             return -1;
-        return a;
+        return dp[n];
     }
 };
