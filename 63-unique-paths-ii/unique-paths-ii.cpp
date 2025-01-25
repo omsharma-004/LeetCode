27a93da334cf13004bd51c316b22cd5a24a50f60
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-25 11:11:27 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (54.58%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        if (grid[0][0] == 1 || grid[n - 1][m - 1] == 1) {
+            return 0;
+        }
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        dp[0][0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (grid[i][0] == 0 && dp[i - 1][0] == 1) {
+                dp[i][0] = 1;
+            } else {
+                dp[i][0] = 0;
+            }
+        }
+        for (int j = 1; j < m; j++) {
+            if (grid[0][j] == 0 && dp[0][j - 1] == 1) {
+                dp[0][j] = 1;
+            } else {
+                dp[0][j] = 0;
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (grid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+};
