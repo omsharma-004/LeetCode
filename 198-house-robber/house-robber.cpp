728046bc3e6244f3d0f9b8bb8e3b9b37093fd1fd
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-25 09:59:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (77.72%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return nums[0];
+        int dp[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = nums[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1]);
+            dp[i][1] = dp[i - 1][0] + nums[i];
+        }
+        return max(dp[n - 1][0], dp[n - 1][1]);
+    }
+};
