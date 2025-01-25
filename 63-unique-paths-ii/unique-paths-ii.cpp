d4219f78da07a8a9ff47490cd487d32c07477f73
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-25 11:11:55 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (28.94%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -9,18 +9,10 @@ public:
         vector<vector<int>> dp(n, vector<int>(m, 0));
         dp[0][0] = 1;
         for (int i = 1; i < n; i++) {
-            if (grid[i][0] == 0 && dp[i - 1][0] == 1) {
-                dp[i][0] = 1;
-            } else {
-                dp[i][0] = 0;
-            }
+            dp[i][0] = (grid[i][0] == 0 && dp[i - 1][0] == 1) ? 1 : 0;
         }
         for (int j = 1; j < m; j++) {
-            if (grid[0][j] == 0 && dp[0][j - 1] == 1) {
-                dp[0][j] = 1;
-            } else {
-                dp[0][j] = 0;
-            }
+            dp[0][j] = (grid[0][j] == 0 && dp[0][j - 1] == 1) ? 1 : 0;
         }
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
