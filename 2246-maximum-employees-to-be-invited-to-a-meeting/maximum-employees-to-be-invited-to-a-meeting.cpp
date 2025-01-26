56095f6951ccfea459a98ead848b554bafd8a091
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-26 15:05:34 +0530

    Time: 22 ms (76.64%) | Memory: 93.2 MB (79.92%) - LeetSync

---
+++ 2246-maximum-employees-to-be-invited-to-a-meeting/maximum-employees-to-be-invited-to-a-meeting.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int maximumInvitations(vector<int>& favorite) {
+        int n = favorite.size();
+        vector<int> depth(n, 1), inDegree(n);
+        queue<int> q;
+
+        for (int i = 0; i < n; ++i)
+            inDegree[favorite[i]]++;
+        for (int i = 0; i < n; ++i)
+            if (!inDegree[i])
+                q.push(i);
+
+        while (!q.empty()) {
+            int cur = q.front();
+            q.pop();
+            depth[favorite[cur]] = max(depth[favorite[cur]], depth[cur] + 1);
+            if (!--inDegree[favorite[cur]])
+                q.push(favorite[cur]);
+        }
+
+        int twoCycle = 0, longest = 0;
+        for (int i = 0; i < n; ++i) {
+            if (!inDegree[i])
+                continue;
+            int len = 0, j = i;
+            while (inDegree[j]) {
+                inDegree[j] = 0;
+                len++;
+                j = favorite[j];
+            }
+            if (len == 2)
+                twoCycle += depth[i] + depth[favorite[i]];
+            else if (len > longest)
+                longest = len;
+        }
+
+        return max(twoCycle, longest);
+    }
+};
