a89be459f0c641e0e7fe5d57e44b0e4f0ff6ffb0
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-29 11:13:40 +0530

    Time: 89 ms (37.54%) | Memory: 94.1 MB (6.84%) - LeetSync

--- 2310-minimum-operations-to-halve-array-sum/minimum-operations-to-halve-array-sum.cpp
+++ 2310-minimum-operations-to-halve-array-sum/minimum-operations-to-halve-array-sum.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int halveArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         priority_queue<double> pq;
         double ts = 0.0;
         for (int i = 0; i < nums.size(); i++) {
             ts += nums[i];
-            pq.push((nums[i] * 1.0));
+            pq.push(nums[i] * 1.0);
         }
         int ans = 0;
         double fs = ts / 2.0;
         while (ts > fs) {
             double a = pq.top();
             pq.pop();
-            ts -= ((a) / 2.0);
-            pq.push((a) / 2.0);
+            ts -= (a / 2.0);
+            pq.push(a / 2.0);
             ans++;
         }
         return ans;
