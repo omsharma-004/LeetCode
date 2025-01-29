9be3474a13889e4f543ee593828e7a33125a9199
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-29 10:27:16 +0530

    Time: 57 ms (64.49%) | Memory: 148 MB (20.37%) - LeetSync

---
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -0,0 +1,24 @@
+#define pp pair<int, pair<int, int>>
+class Solution {
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<vector<int>> v;
+        pp res;
+        priority_queue<pp, vector<pp>, greater<pp>> pq;
+        for(int i=0;i<nums1.size();i++){
+            pq.push({nums1[i]+nums2[0],{i,0}});
+        }
+        while(k-- && !pq.empty()){
+            res=pq.top();
+            pq.pop();
+            int a=res.first;
+            int n1=res.second.first;
+            int n2=res.second.second;
+            v.push_back({nums1[n1], nums2[n2]});
+            if(n2+1<nums2.size()){
+                pq.push({nums1[n1]+nums2[n2+1], {n1,n2+1}});
+            }
+        }
+        return v;
+    }
+};
