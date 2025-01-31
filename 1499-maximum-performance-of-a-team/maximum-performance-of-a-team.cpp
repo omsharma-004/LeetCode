9424f45f0d5e2540d7937609314a5fdb7676c1a0
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-31 09:39:25 +0530

    Time: 23 ms (52.45%) | Memory: 46.4 MB (14.70%) - LeetSync

--- 1499-maximum-performance-of-a-team/maximum-performance-of-a-team.cpp
+++ 1499-maximum-performance-of-a-team/maximum-performance-of-a-team.cpp
@@ -1,26 +1,24 @@
 #define pp pair<long long int, long long int>
-
+bool cmp(pp p1, pp p2) { return p1.first > p2.first; }
 class Solution {
 public:
     int maxPerformance(int n, vector<int>& sp, vector<int>& eff, int k) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        vector<pair<int, int>> v;
+        vector<pp> v;
         for (int i = 0; i < n; i++) {
             v.push_back({eff[i], sp[i]});
         }
-        sort(v.rbegin(), v.rend());
-        priority_queue<int, vector<int>, greater<int>> pq;
-        long long int sum = 0, ans = 0;
+        sort(v.begin(), v.end(), cmp);
+        priority_queue<pp, vector<pp>, greater<pp>> pq;
+        long long int ts = 0;
+        long long int ans = 0;
         for (int i = 0; i < n; i++) {
-            sum += v[i].second;
-            pq.push(v[i].second);
-            if (pq.size() > k) {
-                sum -= pq.top();
+            if (pq.size() == k) {
+                ts -= pq.top().first;
                 pq.pop();
             }
-            ans = max(ans, sum * v[i].first);
+            pq.push({v[i].second, v[i].first});
+            ts += v[i].second;
+            ans = max(ans, ts * v[i].first);
         }
         return ans % 1000000007;
     }
