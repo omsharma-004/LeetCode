dce05dd8827dcb93b2f37a608eeeaff0b49844bd
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-01-31 11:40:08 +0530

    Time: 57 ms (5.15%) | Memory: 13 MB (17.41%) - LeetSync

--- 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.cpp
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int largestSumAfterKNegations(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         sort(nums.begin(), nums.end());
         for(int i=0;i<k;i++){
             nums[0]=-nums[0];
