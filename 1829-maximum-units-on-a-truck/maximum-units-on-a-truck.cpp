2145cf6c24247de7bbbe30a0acc3f83d4f811818
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-02-06 10:16:12 +0530

    Time: 5 ms (51.44%) | Memory: 19.7 MB (88.25%) - LeetSync

---
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
@@ -0,0 +1,22 @@
+bool cmp(vector<int>& a, vector<int>& b){
+    return a[1]>b[1];
+}
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& v, int k) {
+        int res=0;
+        sort(v.begin(), v.end(), cmp);
+        for(int i=0;i<v.size();i++){
+            if(v[i][0]<=k){
+                res+=v[i][0]*v[i][1];
+                k-=v[i][0];
+            }
+            else{
+                res+=k*v[i][1];
+                k=0;
+            }
+            if(k==0) break;
+        }
+        return res;
+    }
+};
