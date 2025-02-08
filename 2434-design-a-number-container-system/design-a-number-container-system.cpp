70013b16f48637de954f7c0c2124b409e14612c8
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-02-08 10:52:05 +0530

    Time: 127 ms (37.50%) | Memory: 235.7 MB (5.00%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/design-a-number-container-system.cpp
@@ -1,21 +1,30 @@
 class NumberContainers {
 public:
-    map<int, set<int>> mpp;
-    map<int, int> mp;
-    NumberContainers() {}
+    unordered_map<int, int> mp;
+    unordered_map<int, set<int>> idx;
+    NumberContainers() { mp.reserve(100000); }
+
     void change(int index, int number) {
-        if (mp[index]) {
-            int val = mp[index];
-            mpp[val].erase(index);
-            mp[index] = number;
-        } else {
-            mp[index] = number;
+        if (mp.count(index)) {
+            int x = mp[index];
+            idx[x].erase(index);
+            if (idx[x].size() == 0)
+                idx.erase(x);
         }
-        mpp[number].insert(index);
+        mp[index] = number;
+        idx[number].insert(index);
     }
+
     int find(int number) {
-        if (mpp[number].empty())
+        if (idx.count(number) == 0)
             return -1;
-        return *(mpp[number].begin());
+        return *(idx[number].begin());
     }
 };
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
