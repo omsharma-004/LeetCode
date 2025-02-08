ae33cfb369f655d928ab65c6af2c6708e7a5f241
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-02-08 10:52:25 +0530

    Time: 91 ms (90.63%) | Memory: 195.8 MB (61.88%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/design-a-number-container-system.cpp
@@ -1,30 +1,37 @@
 class NumberContainers {
 public:
-    unordered_map<int, int> mp;
-    unordered_map<int, set<int>> idx;
-    NumberContainers() { mp.reserve(100000); }
-
-    void change(int index, int number) {
-        if (mp.count(index)) {
-            int x = mp[index];
-            idx[x].erase(index);
-            if (idx[x].size() == 0)
-                idx.erase(x);
-        }
-        mp[index] = number;
-        idx[number].insert(index);
+    unordered_map<int,set<int>> numToIndex;
+    unordered_map<int,int> indexToNum;
+    NumberContainers() {
+        numToIndex.clear();
+        indexToNum.clear();
     }
-
+    
+    void change(int index, int number) {
+        if(indexToNum.find(index)!=indexToNum.end()){
+            int prevNum = indexToNum[index];
+            if(numToIndex.find(prevNum)!=numToIndex.end()){
+                numToIndex[prevNum].erase(index);
+                if(numToIndex[prevNum].size()==0){
+                    numToIndex.erase(prevNum);
+                }
+            }
+        }
+        indexToNum[index]=number;
+        numToIndex[number].insert(index);
+    }
+    
     int find(int number) {
-        if (idx.count(number) == 0)
-            return -1;
-        return *(idx[number].begin());
+        if(numToIndex.find(number)!=numToIndex.end()){
+            return *numToIndex[number].begin();
+        }
+        return -1;
     }
 };
 
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
