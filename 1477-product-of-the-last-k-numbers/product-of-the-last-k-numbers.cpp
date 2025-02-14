2257f11375e9bd740cc2eb158a39c82f1b79f05d
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-02-14 16:56:22 +0530

    Time: 24 ms (51.45%) | Memory: 77.9 MB (77.84%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
@@ -1,15 +1,20 @@
 class ProductOfNumbers {
+    vector<int> preprods;
+
 public:
-    vector<int> arr, mul;
-    ProductOfNumbers() {}
+    ProductOfNumbers() { preprods.push_back(1); }
     void add(int num) {
-        arr.push_back(num);
-        if(mul.empty() && num) mul.push_back(num);
-        else if(!num) mul.clear();
-        else mul.push_back(mul.back() * num);
+        if (num == 0) {
+            preprods.clear();
+            preprods.push_back(1);
+            return;
+        }
+        preprods.push_back(preprods[preprods.size() - 1] * num);
     }
     int getProduct(int k) {
-        if (k > mul.size()) return 0;
-        return k == mul.size() ? mul.back() : mul.back() / mul[mul.size() - k - 1];
+        if (k > preprods.size() - 1)
+            return 0;
+        return preprods[preprods.size() - 1] /
+               preprods[preprods.size() - k - 1];
     }
 };
