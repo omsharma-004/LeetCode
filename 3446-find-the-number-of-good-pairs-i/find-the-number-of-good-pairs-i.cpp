afc4176f916341c4a184000f9000161d73ae201a
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-02-14 20:44:53 +0530

    Time: 1 ms (17.46%) | Memory: 43.1 MB (5.17%) - LeetSync

--- 3446-find-the-number-of-good-pairs-i/find-the-number-of-good-pairs-i.cpp
+++ 3446-find-the-number-of-good-pairs-i/find-the-number-of-good-pairs-i.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        int count=0;
-        for(int i=0; i<nums1.size(); i++){
-            for(int j=0; j<nums2.size(); j++){
-                if(nums1[i] % (nums2[j]*k) == 0)
-                    count++;
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        unordered_map<int, int> mp;
+        for (const auto& n : nums2) {
+            mp[n * k]++;
+        }
+
+        int goodPair = 0;
+        for (int n : nums1) {
+            for (const auto& [d, c] : mp) {
+                if (n % d == 0)
+                    goodPair += c;
             }
         }
-        return count;
+        return goodPair;
     }
 };
