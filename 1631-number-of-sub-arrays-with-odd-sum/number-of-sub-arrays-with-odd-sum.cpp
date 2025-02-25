0ed4405be7f554d8888691aeccfa4442365117eb
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-02-25 10:04:56 +0530

    Time: 4 ms (62.15%) | Memory: 112.1 MB (55.17%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -2,18 +2,21 @@ class Solution {
 public:
     int M = 1e9 + 7;
     int numOfSubarrays(vector<int>& arr) {
-        int odd =0,even=1,ret=0,sum=0;
-        for(int i:arr){
-            sum +=i;
-            if(sum%2){
+        int odd = 0;
+        int even = 1;
+        int ret = 0;
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+            if (sum % 2) {
                 odd++;
-                ret+=even;
-            }else{
+                ret += even;
+            } else {
                 even++;
-                ret+=odd;
+                ret += odd;
             }
-            ret = ret%M;
+            ret = ret % M;
         }
-        return ret%M;
+        return ret % M;
     }
 };
