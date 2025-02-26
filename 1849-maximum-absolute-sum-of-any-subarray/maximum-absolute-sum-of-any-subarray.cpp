ffe65738f0d446ff0fdbd419ff7f0d754aac0432
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-02-26 23:46:03 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (95.34%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
-    int maxAbsoluteSum(vector<int>& nums) 
-    {
-        int possum = INT_MIN, negsum = INT_MAX;
-        int curpos = 0, curneg = 0;
-        
-        for(int n : nums)
-        {
-            curpos += n;
-            curneg += n;
-            
-            possum = max(possum, curpos);
-            negsum = min(negsum, curneg);
-            
-            if (curpos < 0)
-                curpos = 0;
-            if (curneg > 0)
-                curneg = 0;
+    int maxAbsoluteSum(vector<int>& nums) {
+        int sum = 0, minSum = 0, maxSum = 0;
+        for (int num : nums) {
+            sum += num;
+            if (sum > maxSum) maxSum = sum;
+            if (sum < minSum) minSum = sum;
         }
-        
-        return max(possum, -negsum);
+        return abs(maxSum - minSum);
     }
 };
