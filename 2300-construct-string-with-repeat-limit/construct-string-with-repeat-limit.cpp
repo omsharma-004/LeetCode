ee78c2196387c3420262ee3c387426e762c8a35f
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-02-26 23:31:11 +0530

    Time: 41 ms (50.40%) | Memory: 28.1 MB (55.99%) - LeetSync

---
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string repeatLimitedString(string s, int k) {
+        unordered_map<char, int> mp;
+        for(auto i:s){
+            mp[i]++;
+        }
+        priority_queue<pair<char, int>> pq;
+        for(auto i:mp){
+            pq.push(i);
+        }
+        string ans="";
+        while(!pq.empty()){
+            auto largest=pq.top();
+            pq.pop();
+            int len=min(largest.second, k);
+            for(int i=0;i<len;i++){
+                ans+=largest.first;
+            }
+            pair<char, int> slargest;
+            if(largest.second-len>0){
+                if(!pq.empty()){
+                    slargest=pq.top();
+                    pq.pop();
+                    ans+=slargest.first;
+                }
+                else{
+                    return ans;
+                }
+                if(slargest.second-1>0){
+                    pq.push({slargest.first, slargest.second-1});
+                }
+                pq.push({largest.first, largest.second-len});
+            }
+        }
+        return ans;
+    }
+};
