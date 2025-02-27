d47d2b3c666868b4da2bd7de28800f6cca77ad05
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-02-27 15:08:05 +0530

    Time: 275 ms (76.71%) | Memory: 15.8 MB (74.57%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int lenLongestFibSubseq(vector<int>& arr) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         unordered_set<int>s(arr.begin(), arr.end());
         int n = arr.size(), result = 0;
         for (int i = 0; i < n; i++) {
