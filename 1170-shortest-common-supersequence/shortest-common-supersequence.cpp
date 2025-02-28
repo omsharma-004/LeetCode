6500bb6ae960bc4059dab8bdb2bc1ca4358583b0
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-02-28 20:46:00 +0530

    Time: 26 ms (51.61%) | Memory: 26.7 MB (87.72%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,61 +1,50 @@
 class Solution {
-private:
-    void LCS(string &str1, string &str2, vector<vector<int>> &dp){
-        int m = str1.size();
-        int n = str2.size();
-
-        for(int i = 1; i <= m; ++i){
-            for(int j = 1; j <= n; ++j){
-                if(str1[i-1] == str2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
-        }
-    }
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int m = str1.size();
-        int n = str2.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        int n = str1.size();
+        int m = str2.size();
         
-        LCS(str1, str2, dp);
-        // int len = dp[m][n];
-
-        int i = m;
-        int j = n;
+        // Step 1: Compute the Longest Common Subsequence (LCS) DP table
+        vector<vector<int>> memo(n + 1, vector<int>(m + 1, 0));
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (str1[i - 1] == str2[j - 1]) 
+                    memo[i][j] = 1 + memo[i - 1][j - 1];
+                else 
+                    memo[i][j] = max(memo[i - 1][j], memo[i][j - 1]);
+            }
+        }
+        
+        // Step 2: Backtrack to construct the Shortest Common Supersequence
         string ans = "";
-
-        while(i > 0 && j > 0){
-            if(str1[i-1] == str2[j-1]){
-                ans += str1[i-1];
+        int i = n, j = m;
+        
+        while (i > 0 && j > 0) {
+            if (str1[i - 1] == str2[j - 1]) {
+                ans.push_back(str1[i - 1]);
                 i--;
                 j--;
-            }
-            else if(dp[i-1][j] > dp[i][j-1]){
-                ans += str1[i-1];
+            } else if (memo[i - 1][j] >= memo[i][j - 1]) {
+                ans.push_back(str1[i - 1]);
                 i--;
-            }
-            else{
-                ans += str2[j-1];
+            } else {
+                ans.push_back(str2[j - 1]);
                 j--;
             }
         }
-
-        while(i > 0){
-            ans += str1[i-1];
+        
+        // Add remaining characters from str1 and str2
+        while (i > 0) {
+            ans.push_back(str1[i - 1]);
             i--;
         }
-
-        while(j > 0){
-            ans += str2[j-1];
+        while (j > 0) {
+            ans.push_back(str2[j - 1]);
             j--;
         }
-
+        
         reverse(ans.begin(), ans.end());
-
         return ans;
     }
 };
