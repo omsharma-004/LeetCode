5c052091756f5a50a26db79a6b41c3c55d3f78d6
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-03-04 00:22:43 +0530

    Time: 3 ms (81.51%) | Memory: 14 MB (87.46%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         vector<int> dp;
         for (int num : nums) {
             auto it = lower_bound(dp.begin(), dp.end(), num);
