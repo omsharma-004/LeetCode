fcb6ec1fdf776837cd1028a2d58d9cbeed2c8202
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-03-07 20:21:54 +0530

    Time: 212 ms (43.52%) | Memory: 23.3 MB (42.04%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-    vector<int> closestPrimes(int left, int right) {
-        vector<int> v;
-        if (left == 1)
-            left++;
-        for (int i = left; i <= right; i++) {
-            bool ok = true;
-            for (int j = 2; 1LL * j * j <= i; j++) {
-                if (i % j == 0) {
-                    ok = false;
-                    break;
+vector<bool> helper(int right){
+        vector<bool> isPrime(right + 1, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for(int i = 2; i * i <= right; i++){
+            if(isPrime[i]){
+                for(int j = 2; i * j <= right; j++){
+                    isPrime[i * j] = false;
                 }
             }
-            if (ok == true) {
-                v.push_back(i);
+        }
+        return isPrime;
+    }
+    vector<int> closestPrimes(int left, int right) {
+        vector<bool> isPrime = helper(right);
+        vector<int> prime;
+
+        for(int num = left; num <= right; num++){
+            if(isPrime[num]){
+                prime.push_back(num);
             }
         }
-        if (v.size() < 2) {
-            v.clear();
-            v.push_back(-1);
-            v.push_back(-1);
-            return v;
-        }
-        sort(v.begin(), v.end());
-        int dif = INT_MAX;
-        int fast, last;
-        for (int i = 1; i < v.size(); i++) {
-            if (v[i] - v[i - 1] < dif) {
-                fast = v[i - 1];
-                last = v[i];
-                dif = v[i] - v[i - 1];
+
+        int minDiff = INT_MAX;
+        vector<int> result = {-1, -1};
+
+        for(int i = 1; i < prime.size(); i++){
+            int diff = prime[i] - prime[i-1];
+            if(diff < minDiff){
+                minDiff = diff;
+                result = {prime[i-1], prime[i]};
             }
         }
-        return {fast, last};
+        return result;
     }
 };
