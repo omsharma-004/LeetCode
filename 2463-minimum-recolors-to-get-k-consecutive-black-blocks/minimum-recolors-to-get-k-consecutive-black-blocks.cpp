87712d3728c685f5d0d95c666550ae7e503b592a
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-03-08 22:42:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (5.10%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int minimumRecolors(string str, int length) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         const int n = str.size();
         int count = 0;
         for (int i = 0; i < length; ++i)
