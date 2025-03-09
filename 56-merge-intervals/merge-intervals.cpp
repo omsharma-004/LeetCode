3085a5a858cb2ee959cb1eb3f4e9321a8659f08c
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-03-09 19:48:33 +0530

    Time: 7 ms (44.30%) | Memory: 24.9 MB (11.85%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -4,6 +4,9 @@ bool cmp(vector<int>& a, vector<int>& b){
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         vector<vector<int>> ans;
         sort(arr.begin(), arr.end(), cmp);
         int n=arr.size();
