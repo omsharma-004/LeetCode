a023485c13f985a91e2f0f3a268091acd20a9ae9
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-03-11 19:31:20 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (81.05%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n = s.length(), cnt = 0;
-        int l = 0, r = 0, a = 0, b = 0, c = 0;
-        while(r < n) {
-            if(s[r] == 'a') a++;
-            else if(s[r] == 'b') b++;
-            else c++;
-            while(a >= 1 && b >= 1 && c >= 1) {
-                cnt += (n - r);
-                if(s[l] == 'a') a--;
-                else if(s[l] == 'b') b--;
-                else c--;
-                l++; 
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        int i = 0;
+        int j = 0;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int n = s.size();
+        int ans = 0;
+        while (j < s.size()) {
+            if (s[j] == 'a')
+                a++;
+            if (s[j] == 'b')
+                b++;
+            if (s[j] == 'c')
+                c++;
+            while (a && b && c && i < j) {
+                ans += n - j;
+                if (s[i] == 'a')
+                    a--;
+                if (s[i] == 'b')
+                    b--;
+                if (s[i] == 'c')
+                    c--;
+                i++;
             }
-            r++;
+            j++;
         }
-        return cnt;
+        return ans;
     }
 };
